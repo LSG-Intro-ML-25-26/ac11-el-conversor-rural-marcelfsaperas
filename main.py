001ad8b3661a4bf88cb54ca10ef1a6b23d0565b2
Marcel Feliu Saperas <marcel.feliu@gracia.lasalle.cat>
2025-12-18 18:55:51 +0100

    Sprites and tree interaction

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def on_up_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-up
-            """),
-        500,
-        False)
-controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+@namespace
+class SpriteKind:
+    Resource = SpriteKind.create()
+def on_forever():
+    if nena.overlaps_with(arbre):
+        game.show_long_text("You're touching the tree!", DialogLayout.BOTTOM)
 
 def on_left_pressed():
     animation.run_image_animation(nena,
@@ -25,17 +23,77 @@ def on_right_pressed():
         False)
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
-def on_down_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-down
-            """),
-        500,
-        False)
-controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
-
+arbre: Sprite = None
 nena: Sprite = None
+playerCabras = 0
+playerWood = 0
+playerChicken = 0
+playerPotatoes = 0
+playerEggs = 0
+playerHorse = 0
+last_text_time = 0
+text_cooldown = 2000
+scene.set_background_image(assets.image("""
+    seasonalTree1
+    """))
 nena = sprites.create(assets.image("""
     nena-front
     """), SpriteKind.player)
-controller.move_sprite(nena)
+arbre = sprites.create(assets.image("""
+    treePine
+    """), SpriteKind.Resource)
+casa = sprites.create(img("""
+        ....................e2e22e2e....................
+        .................222eee22e2e222.................
+        ..............222e22e2e22eee22e222..............
+        ...........e22e22eeee2e22e2eeee22e22e...........
+        ........eeee22e22e22e2e22e2e22e22e22eeee........
+        .....222e22e22eeee22e2e22e2e22eeee22e22e222.....
+        ...22eeee22e22e22e22eee22eee22e22e22e22eeee22...
+        4cc22e22e22eeee22e22e2e22e2e22e22eeee22e22e22cc4
+        6c6eee22e22e22e22e22e2e22e2e22e22e22e22e22eee6c6
+        46622e22eeee22e22eeee2e22e2eeee22e22eeee22e22664
+        46622e22e22e22eeee22e2e22e2e22eeee22e22e22e22664
+        4cc22eeee22e22e22e22eee22eee22e22e22e22eeee22cc4
+        6c622e22e22eeee22e22e2e22e2e22e22eeee22e22e226c6
+        466eee22e22e22e22e22e2e22e2e22e22e22e22e22eee664
+        46622e22eeee22e22e22e2e22e2e22e22e22eeee22e22664
+        4cc22e22e22e22e22eeee2e22e2eeee22e22e22e22e22cc4
+        6c622eeee22e22eeee22eee22eee22eeee22e22eeee226c6
+        46622e22e22eeee22e22e2e22e2e22e22eeee22e22e22664
+        466eee22e22e22e22e22e2e22e2e22e22e22e22e22eee664
+        4cc22e22eeee22e22e22e2e22e2e22e22e22eeee22e22cc4
+        6c622e22e22e22e22e22eee22eee22e22e22e22e22e226c6
+        46622eeee22e22e22eeecc6666cceee22e22e22eeee22664
+        46622e22e22e22eeecc6666666666cceee22e22e22e22664
+        4cceee22e22eeecc66666cccccc66666cceee22e22eeecc4
+        6c622e22eeecc66666cc64444446cc66666cceee22e226c6
+        46622e22cc66666cc64444444444446cc66666cc22e22664
+        46622cc6666ccc64444444444444444446ccc6666cc22664
+        4ccc6666ccc6444bcc666666666666ccb4446ccc6666ccc4
+        cccccccc6666666cb44444444444444bc6666666cccccccc
+        64444444444446c444444444444444444c64444444444446
+        66cb444444444cb411111111111111114bc444444444bc66
+        666cccccccccccd166666666666666661dccccccccccc666
+        6666444444444c116eeeeeeeeeeeeee611c4444444446666
+        666e2222222e4c16e4e44e44e44e44ee61c4e2222222e666
+        666eeeeeeeee4c16e4e44e44e44e44ee61c4eeeeeeeee666
+        666eddddddde4c66f4e4effffffe44ee66c4eddddddde666
+        666edffdffde4c66f4effffffffff4ee66c4edffdffde666
+        666edccdccde4c66f4effffffffffeee66c4edccdccde666
+        666eddddddde4c66f4eeeeeeeeeeeeee66c4eddddddde666
+        c66edffdffde4c66e4e44e44e44e44ee66c4edffdffde66c
+        c66edccdccde4c66e4e44e44e44e44ee66c4edccdccde66c
+        cc66666666664c66e4e44e44e44feeee66c46666666666cc
+        .c66444444444c66e4e44e44e44ffffe66c44444444466c.
+        ..c64eee4eee4c66f4e44e44e44f44fe66c4eee4eee46c..
+        ...c4eee4eee4c66f4e44e44e44effee66c4eee4eee4c...
+        ....644444444c66f4e44e44e44e44ee66c444444446....
+        .....64eee444c66f4e44e44e44e44ee66c444eee46.....
+        ......6ccc666c66e4e44e44e44e44ee66c666ccc6......
+        """),
+    SpriteKind.player)
+arbre.set_position(128, 68)
+nena.set_position(27, 74)
+casa.set_position(27, 67)
+controller.move_sprite(nena, 100, 0)
---
+++ tilemap.g.ts
@@ -0,0 +1,54 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "forestTiles2":
+            case "tile1":return tile1;
+            case "forestTiles1":
+            case "tile2":return tile2;
+            case "forestTiles3":
+            case "tile3":return tile3;
+            case "forestTiles6":
+            case "tile4":return tile4;
+            case "forestTiles7":
+            case "tile5":return tile5;
+            case "forestTiles9":
+            case "tile6":return tile6;
+            case "forestTiles11":
+            case "tile7":return tile7;
+            case "forestTiles14":
+            case "tile8":return tile8;
+            case "forestTiles5":
+            case "tile9":return tile9;
+            case "myTile":
+            case "tile10":return tile10;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
